f520a3006b348ebaf450d45e0c2135039aaa24d9
Márcio Alves <alvesmarcio@hotmail.com>
2021-10-04 17:20:39 -0300

    1a versao clickEvent

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 
 
-
+const tabuleiro = document.querySelector("#tabuleiro");
 
 // ============================ Functions ======================//
 
@@ -13,3 +13,23 @@
 
 
 // ============================ Listeners ======================//
+
+tabuleiro.addEventListener("click", (evt) => {
+    let disco = document.createElement("div");
+    let contagem = this.childElementCount;
+    console.log(contagem, ' discos nas colunas')
+    let player1 = true;
+    console.log(player1, 'jogador1?')
+
+    if (contagem < 7) {
+        if (player1 == true) {
+            disco.classList.add("player1");
+            currentTarget.appendChild(disco);
+            player1 = false;
+        } else {
+            disco.classList.add("player2");
+            currentTarget.appendChild(disco);
+            player1 = true;
+        }
+    }
+});
