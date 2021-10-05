cba2f8d1bb51eb01937dcc864f16151fcc0c1379
Márcio Alves <alvesmarcio@hotmail.com>
2021-10-05 15:03:15 -0300

    cria eventListener 2

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 // ============================ CONSTS ======================== //
-const board = document.getElementById("tabuleiro")
+const board = document.getElementById("tabuleiro");
 
 const boardSize = {
-    columns: 7,
-    rows: 6
-}
+  columns: 7,
+  rows: 6,
+};
 
-
-
-
-
-
+let player = true;
+let discoAtual
 // ============================ Functions ======================//
 
 const generateBoard = (container, size) => {
-    for (let i = 0; i < size.columns; i++){
-        const row = document.createElement("div")
-        row.classList.add("column")
-        row.id = `C${i}`
-        for (let j = 0; j < size.rows; j++){
-            const cell = document.createElement("div")
-            cell.id = `E${i}${j}`
-            cell.classList.add("cell")
-            row.appendChild(cell)
-        }
-        container.appendChild(row)
+  for (let i = 0; i < size.columns; i++) {
+    const row = document.createElement("div");
+    row.classList.add("column");
+    row.id = `C${i}`;
+    for (let j = 0; j < size.rows; j++) {
+      const cell = document.createElement("div");
+      cell.id = `E${i}${j}`;
+      cell.classList.add("cell");
+      row.appendChild(cell);
     }
-}
+    container.appendChild(row);
+  }
+};
 
-generateBoard(board, boardSize)
-
-
-
+generateBoard(board, boardSize);
 
 // ============================ Listeners ======================//
 
 board.addEventListener("click", (evt) => {
-    let disco = document.createElement("div");
-    let contagem = this.childElementCount;
-    console.log(contagem, ' discos nas colunas')
-    let player1 = true;
-    console.log(player1, 'jogador1?')
+  let coluna = evt.target;
+  if (coluna.classList.contains("cell")) {
+    coluna = coluna.parentNode;
+  };
+  let selecionaDisco = coluna.children;
+  console.log(selecionaDisco.length - 1, 'length')
+  for (let i = selecionaDisco.length - 1; i >= 0; i--) {
+      console.log(i, 'check i')
+      if (selecionaDisco[i].classList.length == 1 && player == true) {
+          selecionaDisco[i].classList.add("player1");
+          player = false;
+          discoAtual = selecionaDisco[i]
 
-    if (contagem < 7) {
-        if (player1 == true) {
-            disco.classList.add("player1");
-            currentTarget.appendChild(disco);
-            player1 = false;
-        } else {
-            disco.classList.add("player2");
-            currentTarget.appendChild(disco);
-            player1 = true;
-        }
-    }
+          break;
+      }
+      if (selecionaDisco[i].classList.length == 1 && player == false) {
+          selecionaDisco[i].classList.add("player2");
+          player = true;
+          discoAtual = selecionaDisco[i];
+
+          break;
+      }
+  }
 });
